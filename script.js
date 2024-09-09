8b86fe778683f18a357e793dd01e2ce8ac1418df
Terrana Willma <terrana.w@gmail.com>
2024-09-09 20:25:02 +0700

    10mnt

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// input        // result
+//[1,4,6,7]       // [14,12,8,2]
+//[7,4,2,1]       // [2,4,8,14]
+//[5]             // [10]
 
+function reverseAndTimes (arr){
+    return arr.reverse().map(num => num*2)
+}
+console
